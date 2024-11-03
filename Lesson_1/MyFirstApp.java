78a50cc35b2a5c0ec3931da04097b8c73328c91d
sloWorker <164147951+sloWorker@users.noreply.github.com>
2024-11-03 10:25:59 +0800

    Changed the text output

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere");
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
         Scanner console = new Scanner(System.in);
         System.out.print("Введите свое имя: ");
         System.out.println(console.nextLine());
